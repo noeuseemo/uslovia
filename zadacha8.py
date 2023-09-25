0e77363f79f7729a43ca34753d5baf18619890be
kirill <143942709+noeuseemo@users.noreply.github.com>
2023-09-25 21:39:45 +0700

    Update zadacha8.py(new)

--- zadacha8.py
+++ zadacha8.py
@@ -3,10 +3,12 @@ year = int(input('введите год: '))
 if year % 4 == 0:
         if year % 100 != 0:
             print('год високосный')
-        else:
+        else
             if year % 400 == 0:
-                print('год не високосный')
+                print('год  високосный')
             else:
                 print('год не високосный')
 else:
         print('год не високосный')
+
+
