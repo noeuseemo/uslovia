e2a0e403bae9e65cc4ae983f00a88b460ccc260a
kirill <143942709+noeuseemo@users.noreply.github.com>
2023-09-30 00:43:05 +0700

    second commit

--- zadacha8.py
+++ zadacha8.py
@@ -3,12 +3,10 @@ year = int(input('введите год: '))
 if year % 4 == 0:
         if year % 100 != 0:
             print('год високосный')
-        else
+        else:
             if year % 400 == 0:
                 print('год  високосный')
             else:
                 print('год не високосный')
 else:
         print('год не високосный')
-
-
