ffacb5d445172abc7484fb4b0c85453b4a4227c1
kirill <143942709+noeuseemo@users.noreply.github.com>
2023-09-30 00:41:32 +0700

    second commit

--- zadacha10.py
+++ zadacha10.py
@@ -1,22 +1,19 @@
-a = int(input('введите коэф.а- '))
-b = int(input('введите коэф.б- '))
-c = int(input('введите коэф с- '))
+ a = int(input('введите а- '))
+    b = int(input('введите б- '))
+    c = int(input('введите с- '))
 
-v = (b**2) - (4*a*c)
+    d = (b**2) - (4*a*c)
 
-if v<0:
-    print('no result')
+    if d < 0:
+        print('no results')
+    elif d == 0:
+        x = round((-b) / (2*a), 2)
+        print(f'x = {x}')
+    else:
+        x1 = round((-b + math.sqrt(d)) / (2*a), 2)
+        x2 = round((-b - math.sqrt(d)) / (2*a), 2)
 
-elif v == 0:
-    x = round((-b) / (2*a), 2)
-    print(f'x = {x}')
-
-else:
-    x1 = round((-b + (v)) / (2*a), 2)
-    x2 = round((-b - (v)) / (2*a), 2)
-
-if x1 == x2:
-    print(f'x1 = x2 = {x1}')
-
-else:
-    print(f'x1 = {x1}\nx2 = {x2}')
+        if x1 == x2:
+            print(f'x1 = x2 = {x1}')
+        else:
+            print(f'x1 = {x1}\nx2 = {x2}')
